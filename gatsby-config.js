7c54521ba3d9d852e1059173a51017b41368a683
Austen Constable <awconstable@gmail.com>
2019-10-14 14:37:31 +0100

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-451873-2"
+      }
+    }
   ],
   // Customize your site metadata:
   siteMetadata: {
