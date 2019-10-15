2c0382a0d67854e721a6b7f96c8424bcb9b660e3
Austen Constable <awconstable@gmail.com>
2019-10-15 12:07:21 +0100

    Added a sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,18 +2,23 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-blog`,
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-451873-2"
       }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {}
     }
   ],
   // Customize your site metadata:
   siteMetadata: {
     title: `Austen's Blog`,
+    siteUrl: `https://austenconstable.com`,
     author: `Austen Constable`,
     description: `A blog of travel, linux and coding`,
     social: [
