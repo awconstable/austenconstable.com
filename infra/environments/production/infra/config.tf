329b271a7ff153171409a4b043260bb9e5b9c4a9
Austen Constable <awconstable@gmail.com>
2022-05-03 17:11:53 +0100

    Switch to terraform cloud and update to 1.1.9

--- infra/environments/production/infra/config.tf
+++ infra/environments/production/infra/config.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket         = "austenconstable.com-production-state-file"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    dynamodb_table = "austenconstable.com-production-state-file-locking"
+  cloud {
+    organization = "austenconstable"
+    workspaces {
+      name = "austenconstable_com-production"
+    }
   }
 }
 
