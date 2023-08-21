f89724e60009ecf53e3e37f3895d7d944a282bd9
Austen Constable <awconstable@gmail.com>
2023-08-21 15:44:34 +0100

    Switch back to s3 state

--- infra/environments/production/infra/config.tf
+++ infra/environments/production/infra/config.tf
@@ -1,9 +1,9 @@
 terraform {
-  cloud {
-    organization = "austenconstable"
-    workspaces {
-      name = "austenconstable_com-production"
-    }
+  backend "s3" {
+    bucket         = "austenconstable.com-production-state-file"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    dynamodb_table = "austenconstable.com-production-state-file-locking"
   }
 }
 
